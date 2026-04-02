9b766254f2000572e0843b142d51ca97b19d773f
Euler-Constant <davidorifa39@gmail.com>
2026-04-02 04:00:14 +0100

    blind implementation of the ROFBS algorithm (no shame T_T)

---
+++ rofbs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FILE_PATH file_path;
+#define PROTECTED_DIRECTORIES protected_directories;
+
+// assume defined function is_saved() that saves the pre-modified directories with extension madifiers;
+// assume record() function that saves the state of a file as backed up or not;
+// assume is_malicious() function that checks the true/false value of a malicious file;
+
+bool void(BackupFile); //assume that this creates a backup file
+
+int CreateBackupFile(file_path, protected_directories){
+  char filename[1024]; //char buffer for the file created?
+  if (file_path >> protected_directories && BackupFile == false){
+    is_saved(true, .tmp);
+    record(true);
+  }
+}
+
+int RestoreBackup(file_path, protected_directories){
+  if (is_malicious(true) && BackupFile(exists)){ 
+    //I'm not sure if this is correct, but take it as it may;
+    // assume rename() function that takes the original file name;
+    rename(file_path);
+  } 
+}
